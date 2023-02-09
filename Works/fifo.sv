30091a2f92e0aead0e07791a1824b87c68665693
Lê Ngọc Minh Thư <116614977+lnmthu1505@users.noreply.github.com>
2023-02-09 12:09:14 +0700

    abcdxyz

--- Works/fifo.sv
+++ Works/fifo.sv
@@ -23,7 +23,7 @@
 module fifo(clk, rst_n, in_fifo, out_fifo, i_push, i_pop, i_flush, is_fifo_empty, is_fifo_full);
     parameter FIFO_WIDTH    = 16;
     parameter FIFO_DEPTH    = 8;
-    parameter BIT_WIDTH     = 4; 
+    parameter BIT_WIDTH     = 3; 
          
     input       clk;
     input       rst_n;
@@ -36,29 +36,22 @@ module fifo(clk, rst_n, in_fifo, out_fifo, i_push, i_pop, i_flush, is_fifo_empty
     output reg     is_fifo_full;
     
     logic [FIFO_WIDTH-1:0] ram [FIFO_DEPTH-1:0];
-    logic [BIT_WIDTH-1:0] rd_pt, wr_pt, pt_run;
+    logic [BIT_WIDTH:0] rd_pt, wr_pt;
     logic re_fifo, we_fifo;
-   
     //assign pt_run   = (wr_pt > rd_pt) ? wr_pt - rd_pt : rd_pt - wr_pt;
     assign re_fifo  = (~is_fifo_empty) & i_pop;
     assign we_fifo  = (~is_fifo_full) & i_push;
     
     //-------------------------------------THRESHOLD-----------------------------------------//
-    always_comb
-    begin
-        if (wr_pt > rd_pt) pt_run = wr_pt - rd_pt;
-        else pt_run = rd_pt - wr_pt;
-    end
     always @(*)
     begin
-       
-       is_fifo_full     =   (pt_run == FIFO_DEPTH);
-       is_fifo_empty    =   (pt_run == 0);
-    end
+       is_fifo_full     =   ({~wr_pt[BIT_WIDTH], wr_pt[BIT_WIDTH-1:0]} == rd_pt);
+       is_fifo_empty    =   (wr_pt == rd_pt);
+    end 
     //----------------------------WRITE: LOAD DATA FROM DTP TO FIFO--------------------------//
     always_ff @(posedge clk)
     begin
-        if(!rst_n || wr_pt == FIFO_DEPTH || i_flush)
+        if(!rst_n  || i_flush)
         begin
             wr_pt <= 0;
         end
@@ -72,7 +65,7 @@ module fifo(clk, rst_n, in_fifo, out_fifo, i_push, i_pop, i_flush, is_fifo_empty
     //-------------------------------READ: LOAD DATA FROM FIFO TO MUX------------------------------//
     always_ff @(posedge clk)
     begin
-        if(!rst_n || rd_pt == FIFO_DEPTH-1 || i_flush)
+        if(!rst_n  || i_flush)
         begin
             rd_pt <= 0;
         end
