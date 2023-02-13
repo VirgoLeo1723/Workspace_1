673ca9e50de43c73db7813c4bf470c4fb2d4fee2
Have no fear, Sins is here <92133811+SinsIsHere@users.noreply.github.com>
2023-02-13 20:55:38 +0700

    Update accumulator_gen.sv

--- Works/Accumulator/Accumulator.srcs/sources_1/new/accumulator_gen.sv
+++ Works/Accumulator/Accumulator.srcs/sources_1/new/accumulator_gen.sv
@@ -1,223 +1,152 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/18/2023 12:35:58 AM
-// Design Name: 
-// Module Name: accumulator_gen
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
-
-module accumulator_gen #(
-    parameter                           FIFO_WIDTH  = 16,
-    parameter                           N_LABEL     = 4, 
-	parameter                           BITS        = 2
+module Accumulator_Gen #(
+    parameter FIFO_WIDTH = 16,
+    parameter N_LABELS = 4
 ) (
-    input                               clk,
-    input                               rst_n,
-    input                               i_is_clf,
-    input                               i_is_accum_fin,
+    input                            clk,
+    input                            rst_n,
+    input                            reset,
+    input                            i_is_clf,
+    input                            i_is_accum_fin,
 
-    input   [N_LABEL-1:0]                  label,
+    input  [N_LABELS-1:0]            label,
 
-    output  [N_LABEL*FIFO_WIDTH-1:0]    o_clf_accum_reg,
-    output  [N_LABEL-1:0]               o_clf_accum_reg_vld,
+    output [FIFO_WIDTH*N_LABELS-1:0] o_clf_accum_reg,
+    output                           o_clf_accum_reg_vld,
 
-    output  [FIFO_WIDTH-1:0]            o_rgs_accum_reg,
-    output                              o_rgs_accum_reg_vld
+    output [FIFO_WIDTH-1:0]          o_rgs_accum_reg,
+    output                           o_rgs_accum_reg_vld
 );
-    
-    // CLASSIFICATION
-    wire                        BYPASS_in;
-    wire    [FIFO_WIDTH-1:0]    B_gen_to_en_clf;
-    wire    [FIFO_WIDTH-1:0]    B_en_to_accum_clf     [0:N_LABEL-1];
-    wire    [FIFO_WIDTH-1:0]    Q_accum_to_reg_clf    [0:N_LABEL-1];
-    assign BYPASS_in        = 0 || ~rst_n;
-    assign B_gen_to_en_clf  = 1 ||  rst_n;
-    
-    genvar index;
+
+    //------------------------------CLASSIFICATION------------------------------//
+
+    reg  [N_LABELS-1:0]    B_clf;
+    wire [FIFO_WIDTH-1:0]  B_get_to_accum_clf [0:N_LABELS-1];
+    wire [FIFO_WIDTH-1:0]  Q_accum_to_reg_clf [0:N_LABELS-1];
+
+    wire                   BYPASS;
+    assign BYPASS = reset;
+
+    reg  [FIFO_WIDTH-1:0]  clf_reg            [0:N_LABELS-1];
+
     generate
-        for (index = 0; index < N_LABEL; index = index + 1) begin
-            enable_clf INST1 (
-                .i_is_clf       (i_is_clf),
-                .i_is_accum_fin (i_is_accum_fin),
-                .label          (label[index]),
-                .B_in           (B_gen_to_en_clf),
-                .B_out          (B_en_to_accum_clf[index])
-            );
+        for (genvar i = 0; i < N_LABELS; i = i + 1) begin
+            always_comb begin : Get_B_clf
+                if (!rst_n || reset) begin
+                    B_clf[i] = 0;
+                end else begin
+                    if (i_is_clf && label[i] == 1 && !i_is_accum_fin) begin
+                        B_clf[i] = 1;
+                    end else begin
+                        B_clf[i] = 0;
+                    end
+                end
+            end
 
-            c_accum_1 INST2 (
-                .B      (B_en_to_accum_clf[index]),
+            assign B_get_to_accum_clf[i] = FIFO_WIDTH'(B_clf[i]);
+
+            c_accum_0 Accum_clf (
+                .B      (B_get_to_accum_clf[i]),
                 .CLK    (clk),
-                .BYPASS (BYPASS_in), 
-                .Q      (Q_accum_to_reg_clf[index])
+                .BYPASS (BYPASS), 
+                .Q      (Q_accum_to_reg_clf[i])
             );
 
-            reg_gen_clf INST3 (
-                .clk            (clk),
-                .rst_n          (rst_n),
-                .i_is_accum_fin (i_is_accum_fin),
-                .label          (label[index]),
-                .i_is_clf       (i_is_clf),
-                .Q_in           (Q_accum_to_reg_clf[index]),
-                .clf_vld        (o_clf_accum_reg_vld[N_LABEL-1-index]),
-                .clf_out        (o_clf_accum_reg[FIFO_WIDTH*(N_LABEL-index)-1:FIFO_WIDTH*(N_LABEL-1-index)])
-            );
+            always_ff @( posedge clk ) begin : Reg_clf
+                if (!rst_n || reset) begin
+                    clf_reg[i] <= 0;
+                end else begin
+                    if (i_is_accum_fin && i_is_clf) begin
+                        clf_reg[i] <= Q_accum_to_reg_clf[i];
+                    end
+                end
+            end
+
+            assign o_clf_accum_reg[FIFO_WIDTH*(N_LABELS-i)-1:FIFO_WIDTH*(N_LABELS-1-i)] = clf_reg[i];
         end
     endgenerate
 
-    // REGRESSION
-    reg     [FIFO_WIDTH-1:0]    B_gen_to_en_rgs;
-    wire    [FIFO_WIDTH-1:0]    B_en_to_accum_rgs;
-    wire    [FIFO_WIDTH-1:0]    Q_accum_to_reg_rgs;
-    integer pos;
-    always @(*) begin
-        if (!rst_n)
-            B_gen_to_en_rgs = 0;
-        else begin
-            if (label==0) B_gen_to_en_rgs = 0;
-            else begin
-                for (pos=0; pos<N_LABEL; pos=pos+1) begin
-                    if (label[pos]==1'b1) B_gen_to_en_rgs = pos + 1;        
+    //--------------------------------------------------------------------------//
+
+
+
+
+
+    //--------------------------------REGRESSION--------------------------------//
+
+    reg  [FIFO_WIDTH-1:0] B_rgs;
+    //wire [FIFO_WIDTH-1:0] B_get_to_accum_rgs;
+    wire [FIFO_WIDTH-1:0] Q_accum_to_reg_rgs;
+
+    reg  [FIFO_WIDTH-1:0] rgs_reg;
+
+    always_comb begin : Get_B_rgs
+        if (!rst_n || reset) begin
+            B_rgs = 0;
+        end else begin
+            for (integer i = 0; i < N_LABELS; i = i + 1) begin
+                if (label[i] == 1 && !i_is_clf && !i_is_accum_fin) begin
+                    B_rgs = i + 1;
+                end else begin
+                    B_rgs = 0;
                 end
             end
         end
     end
-    
-    enable_rgs INST4 (
-        .i_is_clf(i_is_clf),
-        .i_is_accum_fin(i_is_accum_fin),
-        .B_in(B_gen_to_en_rgs),
-        .B_out(B_en_to_accum_rgs)
-    );
-    c_accum_1 INST5 (
-        .B      (B_en_to_accum_rgs),
+
+    //assign B_get_to_accum_rgs = B_rgs;
+
+    c_accum_0 Accum_rgs (
+        .B      (B_rgs),
         .CLK    (clk),
-        .BYPASS (BYPASS_in), 
+        .BYPASS (BYPASS), 
         .Q      (Q_accum_to_reg_rgs)
     );
-    reg_gen_rgs INST6 (
-        .clk            (clk),
-        .rst_n          (rst_n),
-        .i_is_accum_fin (i_is_accum_fin),
-        .i_is_clf       (i_is_clf),
-        .Q_in           (Q_accum_to_reg_rgs),
-        .rgs_vld        (o_rgs_accum_reg_vld),
-        .rgs_out        (o_rgs_accum_reg)
-    );
-    
 
-endmodule
-
-
-module enable_clf #(
-	parameter						FIFO_WIDTH	= 16
-)	(
-	input							i_is_clf,
-    input                           i_is_accum_fin,
-	input							label,
-	input		[FIFO_WIDTH-1:0]	B_in,
-
-
-	output reg	[FIFO_WIDTH-1:0]	B_out
-);
-	always @(*) begin 
-		if (i_is_clf == 1'b1 && label == 1'b1 && i_is_accum_fin == 0) begin
-			B_out = B_in;
-		end else begin
-			B_out = 0;
-		end
-	end
-endmodule
-
-module enable_rgs #(
-	parameter						FIFO_WIDTH	= 16
-)	(
-	input							i_is_clf,
-    input                           i_is_accum_fin,
-	input		[FIFO_WIDTH-1:0]	B_in,
-
-
-	output reg	[FIFO_WIDTH-1:0]	B_out
-);
-	always @(*) begin 
-		if (i_is_clf == 0 && i_is_accum_fin == 0) begin
-			B_out = B_in;
-		end else begin
-			B_out = 0;
-		end
-	end
-endmodule
-
-
-module reg_gen_clf #(
-	parameter						FIFO_WIDTH	= 16
-) (
-    input                           rst_n,
-	input							clk,
-	input							i_is_accum_fin,
-	input							label,
-	input							i_is_clf,
-	input	[FIFO_WIDTH-1:0]		Q_in,
-
-
-	output	reg						clf_vld,
-	output	reg	[FIFO_WIDTH-1:0]	clf_out
-);
-	always @(posedge clk) begin
-        if (rst_n == 0) begin
-            clf_out <= 0;
-			clf_vld <= 0;
+    always_ff @( posedge clk ) begin : Reg_rgs
+        if (!rst_n || reset) begin
+            rgs_reg <= 0;
         end else begin
-            if (clf_vld == 1) clf_vld <= 0;
-            if (i_is_accum_fin) begin
-                clf_out <= Q_in;
-                clf_vld <= label & i_is_clf;
+            if (i_is_accum_fin && !i_is_clf) begin
+                rgs_reg <= Q_accum_to_reg_rgs;
             end
         end
-	end
-endmodule
+    end
+
+    assign o_rgs_accum_reg = rgs_reg;
+
+    //--------------------------------------------------------------------------//
 
 
 
 
-module reg_gen_rgs #(
-	parameter						FIFO_WIDTH	= 16
-) (
-    input                           rst_n,
-	input							clk,
-	input							i_is_accum_fin,
-	input							i_is_clf,
-	input	[FIFO_WIDTH-1:0]		Q_in,
+    //--------------------------------REGS VALID--------------------------------//
 
+    reg flag;
+    reg clf_reg_vld;
+    reg rgs_reg_vld;
 
-	output	reg						rgs_vld,
-	output	reg	[FIFO_WIDTH-1:0]	rgs_out
-);
-	always @(posedge clk) begin 
-        if (rst_n == 0) begin
-            rgs_out <= 0;
-            rgs_vld <= 0;
+    always_ff @( posedge clk ) begin : Reg_vld
+        if (!rst_n || reset) begin
+            clf_reg_vld <= 0;
+            rgs_reg_vld <= 0;
+            flag <= 0;
         end else begin
-            if (rgs_vld == 1) rgs_vld <= 0;
-            if (i_is_accum_fin) begin
-                rgs_out <= Q_in;
-                rgs_vld <= ~i_is_clf;
+            if (!i_is_accum_fin) begin
+                flag <= 0;
+            end
+            if (flag) begin
+                clf_reg_vld <= 0;
+                rgs_reg_vld <= 0;
+            end else if (i_is_accum_fin) begin
+                clf_reg_vld <= i_is_clf;
+                rgs_reg_vld <= ~i_is_clf;
+                flag = 1;
             end
         end
-	end
+    end
+
+    assign o_clf_accum_reg_vld = clf_reg_vld;
+    assign o_rgs_accum_reg_vld = rgs_reg_vld;
+
+    //--------------------------------------------------------------------------//
 endmodule
